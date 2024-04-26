6505e1597ddb78b1de043cc4d8bd535c8078f1a2
JACKT72xp <48709035+JACKT72xp@users.noreply.github.com>
2024-04-26 16:49:00 +0200

    Tryouts gorelease

--- multims.rb
+++ multims.rb
@@ -1,24 +1,23 @@
 class Multims < Formula
-    desc "Multims is a CLI tool for managing Kubernetes"
-    homepage "https://github.com/jackt72xp/multims"
-    
-    if Hardware::CPU.intel?
-      url "https://github.com/JACKT72xp/multims/releases/download/v1.0.2/multims-darwin-amd64.tar.gz"
-      sha256 "9c79a22d25bf1ac740b91855816be512eb5ea7c02295817418b7189aaf0fe7eb"
-    elsif Hardware::CPU.arm?
-      url "https://github.com/JACKT72xp/multims/releases/download/v1.02/multims-darwin-arm64.tar.gz"
-      sha256 "fd8b36080c39294b6c4becdd36cf770fd956c376c0b01f0da4b256910eb4b4be"
-    end
-    
-    depends_on "fswatch"
-    
+    desc "MultiMS is a tool to manage multiple Minecraft servers on a single machine."
+    homepage "https://github.com/JACKT72xp/multims"
+    url "https://github.com/JACKT72xp/multims/archive/refs/tags/v1.0.11.tar.gz"
+    sha256 "c3b808ba36c8c4da58cd80a270722ebd2b688690031501d2b159c0b1457d7b02"
+    version "1.0.11"
+  
     def install
-      # Instala el binario 'multims' en /usr/bin
-      bin.install "multims-darwin-#{Hardware::CPU.arch}" => "multims"
-      # Instala el directorio 'templates' dentro del directorio de la fórmula
-      (share/"multims/templates").install "templates"
-    end
-    
+        # Change into the directory containing the project
+        cd "multims-1.0.11" do  # Adjust based on actual directory name
+          system "make"  # Run make with no specific target
+        end
+      
+        # Install the binary
+        arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+        bin.install "dist/multims_darwin_#{arch}/multims" => "multims"
+      end
+      
+      
+  
     test do
       system "#{bin}/multims", "--version"
     end
