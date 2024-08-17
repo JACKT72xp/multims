e00268714add4ede8b4a818f4f5d870f4951f203
JackT72XP <jack.torpoco@optimum-vector.com>
2024-08-18 01:52:32 +0200

    Add multims formula

--- Formula/multims.rb
+++ Formula/multims.rb
@@ -1,12 +1,12 @@
 class Multims < Formula
   desc "Multims is a tool for managing and syncing Kubernetes configurations"
-  homepage "https://github.com/jacktorpoco/multims"
+  homepage "https://github.com/JACKT72xp/multims"
   version "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jacktorpoco/multims/releases/download/v1.0.0/multims_arm64"
-      sha256 "..."  # SHA256 del binario para arm64
+      url "https://github.com/JACKT72xp/multims/releases/download/v1.0.0/multims_arm64"
+      sha256 "..."  # Reemplaza con el valor SHA256 del binario ARM
 
       def install
         bin.install "multims_arm64" => "multims"
@@ -14,8 +14,8 @@ class Multims < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jacktorpoco/multims/releases/download/v1.0.0/multims_amd64"
-      sha256 "..."  # SHA256 del binario para amd64
+      url "https://github.com/JACKT72xp/multims/releases/download/v1.0.0/multims_amd64"
+      sha256 "..."  # Reemplaza con el valor SHA256 del binario AMD64
 
       def install
         bin.install "multims_amd64" => "multims"
